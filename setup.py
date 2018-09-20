239e7d3b0fb4d6fac5e6e24b0077d3c2c7d4bbb6
Grimmer <grimmer0125@gmail.com>
2018-09-20 11:18:19 +0800

    Rename package to termtitle

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="terminaltitle",
+    name="termtitle",
     version="1.0",
     packages=find_packages(),
     entry_points = {
         'console_scripts': [
-            "terminaltitle = changetitle.changetitle:change_title"
+            "termtitle = termtitle.termtitle:change_title"
         ]
     }
 )
