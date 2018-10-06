9450afcadc6439aa20508ecd490443ec4e8d4f1f
Grimmer <grimmer0125@gmail.com>
2018-10-07 01:04:20 +0800

    Print usage example when no title parameter

--- termtitle/termtitle.py
+++ termtitle/termtitle.py
@@ -3,12 +3,13 @@
 
 import sys
 
-def change_title():
-    title = ""
+def change_title():    
     if len(sys.argv) >= 2:
         title = sys.argv[1]
-    # http://tldp.org/HOWTO/Xterm-Title-3.html xterm escape sequences
-    # \033]0; "\033 is an escape code (ESC)". But \u001b or \x1b represent ESC too.
-    # \007 bell char
-    cmd = "\033]0;{}\007".format(title)
-    sys.stdout.write(cmd)
+        # http://tldp.org/HOWTO/Xterm-Title-3.html xterm escape sequences
+        # \033]0; "\033 is an escape code (ESC)". But \u001b or \x1b represent ESC too.
+        # \007 bell char
+        cmd = "\033]0;{}\007".format(title)
+        sys.stdout.write(cmd)
+    else:
+        print("Usage: termtitle NEW_TITLE")    
