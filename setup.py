cbb95bcc12b6fef6fce1f546e25449e7535a6935
Grimmer <grimmer0125@gmail.com>
2021-04-21 12:43:35 +0800

    Tmp commit

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ with io.open('README.rst', 'rt', encoding='utf8') as f:
 
 setup(
     name="termtitle",
-    version="0.1.9",
-    description = 'A command line program to change macOS terminal title',
+    version="0.2.0",
+    description='A command line program to change macOS terminal title',
     long_description=readme,
+    # long_description=README,
+    # long_description_content_type="text/markdown",
+    # enable ~/.zshrc DISABLE_AUTO_TITLE=”true” in .zshrc
+    # https://github.com/pypa/warehouse/issues/2170
+    # https://realpython.com/pypi-publish-python-package/
     url='https://github.com/grimmer0125/terminal-title-change',
     author='Grimmer',
     author_email='grimmer0125@gmail.com',
     license='MIT',
     packages=find_packages(),
-    entry_points = {
+    entry_points={
         'console_scripts': [
             "termtitle = termtitle.termtitle:change_title"
         ]
